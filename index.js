80a4791982f543b90f8391dfcfad747845355982
Joris Calvat <jcalvat@gmail.com>
2018-05-24 06:33:11 +0200

    secured server side api calls with md5

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
 const express = require('express')
 const app = express()
+const conf = require('./conf')
+const axios = require('axios')
+const md5 = require('md5')
 
 app.use(express.static('public'))
 
+const getApiParams = () => {
+  const timestamp = new Date().getTime()
+  const toHash =  `${timestamp}${conf.API_PRIVATE}${conf.API_PUBLIC}`
+  const hash = md5(toHash)
+  return {
+    ts: timestamp,
+    apikey: conf.API_PUBLIC,
+    hash: hash
+  }
+}
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+
+app.get('/api/characters', function (req, res) {
+  axios.get(`${conf.BASE_URL}/v1/public/characters`, {
+    params: getApiParams()
+  })
+  .then(function (response) {
+    res.send(response.data)
+  })
+  .catch(function (error) {
+    res.send(error);
+  });
+})
+
+app.get('/api/characters/:id', function (req, res) {
+  axios.get(`${conf.BASE_URL}/v1/public/characters/${req.params.id}`, {
+    params: getApiParams()
+  })
+  .then(function (response) {
+    res.send(response.data)
+  })
+  .catch(function (error) {
+    res.send(error);
+  });
 })
 
 app.listen(3000, function () {
